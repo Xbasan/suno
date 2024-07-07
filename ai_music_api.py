1ae07e6888b79dc0bc4b7eed8a1cdfce7e7c3eff
Xbasan <xamzpok@gmail.com>
2024-07-07 12:22:53 +0300

    Коментарии на API

--- ai_music_api.py
+++ ai_music_api.py
@@ -2,7 +2,6 @@ import json
 import time
 import requests
 
-
 class api:
     def __init__(self):
         self.res_json = {}
@@ -30,7 +29,7 @@ class api:
         continue_at = '', 
         callback_url = ''          
     ):
-
+        
         self.prompt_json['title'] = title
         self.prompt_json['prompt'] = prompt
         self.prompt_json['custom_mode'] = custom_mode
@@ -40,22 +39,21 @@ class api:
         self.prompt_json['continue_at'] = continue_at
         self.prompt_json['callback_url'] = callback_url
 
+    # Отвечает за генерацию токена
+    # Responsible for token generation    
     def token(self,):
         email_json = {"userEmail":"hamzatganukae@gmail.com"}
         token_rec = requests.post(url="https://udioapi.pro/api/token-g", json=email_json).json()
         self.prompt_json['token'] = token_rec['token']
         print(self.prompt_json['token'])
         
-            # self.prompt_json['token'] = token
-            # print(11)
-
+    # Отвечает за выбор жанра музыки
+    # Responsible for choosing the genre of music
     def gpt_prompt(self, gpt_description_prompt):
         self.prompt_json['gpt_description_prompt'] = gpt_description_prompt
 
-        print(
-            self.prompt_json['gpt_description_prompt']
-        )
-
+    # Делает запрос для генерации музыки
+    # Makes a request to generate music
     def ai_requsts(self):                
         res = requests.post(url=self.api_url, json = self.prompt_json)
 
@@ -64,19 +62,22 @@ class api:
             pass
         else:
             self.res_json = res.json() 
-            print(self.res_json)           
+            # print(self.res_json)           
         
-     
+    # Генерирует ответ с ссылкой на музыку
+    # Generates a response with a link to the music
+    
+    # НУЖНО СДЕЛАТЬ АСИНХРОННОЙ
+    # NEEDS TO BE MADE ASYNCHRONOUS
     def res_music(self):
         # time.sleep(90)
-        print("запрос")
+        # print("запрос")
                 
         res_api_url = 'https://udioapi.pro/api/feed?workId='
 
         while True:
             res_api_json = requests.post(url=f'{ res_api_url }{ self.res_json['workId'] }').json()
-            # res_api_json = requests.post(url=f'{ res_api_url }KNH2b7uhXT7lvrZnxPRWG').json()
-            
+            # res_api_json = requests.post(url=f'{ res_api_url }KNH2b7uhXT7lvrZnxPRWG').json()            
             try:
                 if json.loads(res_api_json['data'][-1]['info'])['data']['data'][0]['audio_url'] != '':
                     
@@ -95,12 +96,8 @@ class api:
                     time.sleep(60)
             except KeyError:
                 time.sleep(60)            
-                    
-        # info = json.loa(res_api_json.json()['data']['1']['info'])['data']['data']
-               
-        # print(info['audio_url'])
 
-def test():
+def main():
     ap = api()
     ap.token()
     ap.create('заголовак' ,input("Введи текст песни  ") )
@@ -109,4 +106,4 @@ def test():
     
     
 if __name__ == "__main__":
-    test()
+    main()
